d3893183f4b548bcc4b8cdf431446040c716f781
brunneis <dev@brunneis.com>
2021-02-12 09:47:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
-ipfs init --profile flatfs
-ipfs config profile apply server
-sed -i 's/127.0.0.1/0.0.0.0/g' /root/.ipfs/config
-rm -f /root/.ipfs/repo.lock
+#!/bin/bash
+CONFIG_FILE=/root/.ipfs/config
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  ipfs init --profile flatfs
+  ipfs config profile apply server
+  sed -i 's/127.0.0.1/0.0.0.0/g' $CONFIG_FILE
+else
+  rm -f /root/.ipfs/repo.lock
+fi
+
 ipfs id
 ipfs daemon --enable-gc
